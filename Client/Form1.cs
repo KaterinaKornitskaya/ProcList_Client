9de18452672339df3d3929ca7e9d4dc89ce3159d
Радченко Катерина <ketrinradchenko@gmail.com>
2023-10-17 00:21:31 +0300

    Add project files.

---
+++ Client/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        MyConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MyConnection();
+            label1.Text = "Чтобы получить список процессов сервера \n- нажмите кнопку Send Hi";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.ConnWithServer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.FunSendHi();
+            listBox1.DataSource = conn.GetProc().Result.ToList();
+        }
+    }
+}
---
+++ Client/MyConnection.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    internal class MyConnection
+    {
+        public TcpClient client= new TcpClient(IPAddress.Loopback.ToString(), 49154);
+        // метод Соединится с сервером
+        public async void ConnWithServer()
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    //client = new TcpClient(IPAddress.Loopback.ToString(), 49154);
+                    client.Connect(IPAddress.Loopback.ToString(), 49154);
+                    MessageBox.Show("Client say1: Подключение установлено");
+                }
+                catch (SocketException ex)
+                {
+                    MessageBox.Show("Client say2: " + ex.Message);
+                }
+
+            });
+           
+        }
+
+        // метод Отправить Hi
+        // клиент просто нажмет кнопку Send Hi, серверу отправится автоматически Hi,
+        // и в ответ сервер отправит список процессов
+        public void FunSendHi()
+        {
+            try
+            {
+                string mes = "Hi";
+                byte[] msg = Encoding.Default.GetBytes(mes);
+                NetworkStream netstream = client.GetStream();
+                
+               
+                netstream.Write(msg, 0, msg.Length);
+                netstream.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Client say3: " + ex.Message);
+            }
+        }
+
+        // метод для получения списка процессов от сервера
+        public async Task<Dictionary<int,string>> GetProc()
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            await Task.Run(() =>
+            {
+                try
+                {
+                    NetworkStream netstream = client.GetStream();
+                    byte[] arr = new byte[client.ReceiveBufferSize];
+                    int len = netstream.Read(arr, 0, client.ReceiveBufferSize);
+                    if (len > 0)
+                    {
+                        MemoryStream stream = new MemoryStream(arr);
+                        BinaryFormatter formatter = new BinaryFormatter();
+
+                        dic = (Dictionary<int, string>)formatter.Deserialize(stream);
+                       // stream.Close();
+                    }
+                    
+                  //  netstream.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Client say4: " + ex.Message);
+                }
+
+            });
+            return dic;
+        }
+    }
+}
